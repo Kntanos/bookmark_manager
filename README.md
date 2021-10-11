e2edf780088a75be2b2583a6ae3995779fd47360
Kostas Ntanos <kostas@ntanos.co.uk>
2021-10-11 20:08:47 +0100

    Setting up a Testing Environment

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
+### To set up the database
+Connect to `psql` and create the `bookmark_manager` and the `bookmark_manager_test` databases:
+
 ```
 CREATE DATABASE bookmark_manager;
+CREATE DATABASE "bookmark_manager_test";
 ```
-To set up the apporpriate tables, connect to the database in `psql` and run the SQL scripts in the `db/migrations` folder in the given order.
+To set up the apporpriate tables, connect to each database in `psql` and run the SQL scripts in the `db/migrations` folder in the given order.
--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -1,3 +1,5 @@
+require 'pg'
+
 feature 'Viewing bookmarks' do
 
   scenario 'visiting the index page' do
@@ -6,6 +8,12 @@ feature 'Viewing bookmarks' do
   end
 
   scenario 'a user can see bookmarks' do
+    connection = PG.connect(dbname: 'bookmark_manager_test')
+
+    connection.exec("INSERT INTO bookmarks VALUES(1, 'http://www.makersacademy.com');")
+    connection.exec("INSERT INTO bookmarks VALUES(2, 'http://www.destroyallsoftware.com');")
+    connection.exec("INSERT INTO bookmarks VALUES(3, 'http://www.google.com');")
+
     visit('/bookmarks')
 
     expect(page).to have_content "http://www.makersacademy.com"
